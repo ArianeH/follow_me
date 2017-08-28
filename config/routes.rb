58ea260cd8c590be3f1d249770dbd4886ec2040e
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-08-28 18:20:00 +0200

    Changed users to guides

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :users, only: [:index, :show, :new, :create, :edit, :update] do
+  resources :users, only: [:index, :show, :new, :create, :edit, :update]
+
+  resources :guides do
     resources :tours do
       resources :bookings
     end
