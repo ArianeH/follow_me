0b11321c4f6f0327b128db45bd262b4afb27e614
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-08-29 17:48:51 +0200

    Removed guides table and role column

--- db/migrate/20170828143933_create_guides.rb
+++
@@ -1,8 +0,0 @@
-class CreateGuides < ActiveRecord::Migration[5.0]
-  def change
-    create_table :guides do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20170828145042_add_guide_to_tours.rb
+++ db/migrate/20170828145042_add_guide_to_tours.rb
@@ -1,5 +1,5 @@
 class AddGuideToTours < ActiveRecord::Migration[5.0]
   def change
-    add_reference :tours, :guide, foreign_key: true
+    add_column :tours, :guide_id, :integer, foreign_key: true
   end
 end
