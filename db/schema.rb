f2b8803912689cab24894af18bcc3ef5219fd72b
Laura Pesce <laurapesce01@gmail.com>
2017-08-30 14:57:50 +0200

    finished-styling-homepage

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 20170828170103) do
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
 
+  create_table "guides", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tours", force: :cascade do |t|
     t.string   "city"
     t.string   "address"
@@ -33,6 +38,7 @@ ActiveRecord::Schema.define(version: 20170828170103) do
     t.datetime "updated_at",  null: false
     t.integer  "attendants"
     t.integer  "guide_id"
+    t.index ["guide_id"], name: "index_tours_on_guide_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -59,4 +65,5 @@ ActiveRecord::Schema.define(version: 20170828170103) do
 
   add_foreign_key "bookings", "tours"
   add_foreign_key "bookings", "users"
+  add_foreign_key "tours", "guides"
 end
