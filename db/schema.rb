ad5f283cbb256c9ba8d5a7128ad680def7b141c8
Christine de la Fuente <c.delafuente88@gmail.com>
2017-08-30 16:06:37 +0200

    fix profile display for nil photo

--- db/schema.rb
+++ db/schema.rb
@@ -40,11 +40,6 @@ ActiveRecord::Schema.define(version: 20170830103142) do
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
 
-  create_table "guides", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "tours", force: :cascade do |t|
     t.string   "city"
     t.string   "address"
@@ -53,13 +48,9 @@ ActiveRecord::Schema.define(version: 20170830103142) do
     t.datetime "updated_at",  null: false
     t.integer  "attendants"
     t.integer  "guide_id"
-
-    t.index ["guide_id"], name: "index_tours_on_guide_id", using: :btree
-
-    t.integer  "price"
     t.float    "latitude"
     t.float    "longitude"
-
+    t.integer  "price"
   end
 
   create_table "users", force: :cascade do |t|
@@ -86,5 +77,4 @@ ActiveRecord::Schema.define(version: 20170830103142) do
 
   add_foreign_key "bookings", "tours"
   add_foreign_key "bookings", "users"
-  add_foreign_key "tours", "guides"
 end
