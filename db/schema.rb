6f8ebfe0b9662a95a55af01b760aa87db6c86790
kajsa kristersson <k.kristersson@gmail.com>
2017-08-30 13:33:45 +0200

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170830103142) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,9 +48,9 @@ ActiveRecord::Schema.define(version: 20170830103142) do
     t.datetime "updated_at",  null: false
     t.integer  "attendants"
     t.integer  "guide_id"
-    t.integer  "price"
     t.float    "latitude"
     t.float    "longitude"
+    t.integer  "price"
   end
 
   create_table "users", force: :cascade do |t|
