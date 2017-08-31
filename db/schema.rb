02fa3ed03a1c7d56c78150768e0ccacd0532a8c1
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-08-31 10:20:05 +0200

    Added to seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170830125128) do
+ActiveRecord::Schema.define(version: 20170830160658) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,10 +64,10 @@ ActiveRecord::Schema.define(version: 20170830125128) do
     t.datetime "updated_at",  null: false
     t.integer  "attendants"
     t.integer  "guide_id"
+    t.integer  "price"
     t.float    "latitude"
     t.float    "longitude"
-    t.integer  "price"
-    t.index ["guide_id"], name: "index_tours_on_guide_id", using: :btree
+    t.string   "name"
   end
 
   create_table "users", force: :cascade do |t|
