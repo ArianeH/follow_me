6eeeeebc9eae1b58149300b7b9317a4343b49f6c
Christine de la Fuente <c.delafuente88@gmail.com>
2017-08-31 12:09:09 +0200

    added Interest 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170830160658) do
-
+ActiveRecord::Schema.define(version: 20170830172502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,9 +73,9 @@ ActiveRecord::Schema.define(version: 20170830160658) do
     t.datetime "updated_at",  null: false
     t.integer  "attendants"
     t.integer  "guide_id"
-    t.integer  "price"
     t.float    "latitude"
     t.float    "longitude"
+    t.integer  "price"
     t.string   "name"
   end
 
