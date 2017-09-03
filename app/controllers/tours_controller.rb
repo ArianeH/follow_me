0f612007ff5e580713c65e9560060c4f41285cd5
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-09-03 22:20:47 +0200

    Added participants

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -7,9 +7,16 @@ class ToursController < ApplicationController
   def show
   	@tour = Tour.find(params[:id])
     @available_visits = @tour.visits.select do |visit|
-      amount_of_bookings = visit.bookings.count do |b|
+
+      confirmed_bookings = visit.bookings.select do |b|
         b.confirmed == true
       end
+
+       amount_of_bookings = 0
+       confirmed_bookings.each do |b|
+          amount_of_bookings += b.participants
+       end
+
       amount_of_bookings < @tour.attendants
     end
 
