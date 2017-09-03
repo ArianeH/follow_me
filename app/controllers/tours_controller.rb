9ee8b63db7aeefa0951597d5ed140ce9fc548a27
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-09-03 12:08:20 +0200

    Added evaluation of available spots

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -6,7 +6,12 @@ class ToursController < ApplicationController
 
   def show
   	@tour = Tour.find(params[:id])
-
+    @available_visits = @tour.visits.select do |visit|
+      amount_of_bookings = visit.bookings.count do |b|
+        b.confirmed == true
+      end
+      amount_of_bookings < @tour.attendants
+    end
     @hash = Gmaps4rails.build_markers(@tour) do |tour, marker|
       marker.lat tour.latitude
       marker.lng tour.longitude
