182672b4e454bb73a721e8696999159f38cc71a9
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-09-03 15:02:37 +0200

    Added participants column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170831114854) do
+ActiveRecord::Schema.define(version: 20170903130100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,10 +32,11 @@ ActiveRecord::Schema.define(version: 20170831114854) do
 
   create_table "bookings", force: :cascade do |t|
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.integer  "visit_id"
     t.boolean  "confirmed"
+    t.integer  "participants"
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
     t.index ["visit_id"], name: "index_bookings_on_visit_id", using: :btree
   end
