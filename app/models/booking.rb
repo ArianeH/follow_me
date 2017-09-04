e637d6f9ed94d4daedfa1311cae2ba135a875edb
kajsa kristersson <k.kristersson@gmail.com>
2017-09-04 19:45:33 +0200

    added validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,12 +2,4 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :visit
   has_many :messages
-
-
-  # def participant_evaluation
-  #   spots_left = @tour.attendants - one_visit.bookings.where(confirmed: true).sum{|b| b.participants})
-  #   if spots_left > @booking.participants
-  # end
-
-
 end
