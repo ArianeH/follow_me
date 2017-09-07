5427278784495e3ce755eda65e4246894ba17199
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-09-07 19:01:16 +0200

    Fixed automatic update acceptance

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,21 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :visit
   has_many :messages, dependent: :destroy
+
+  after_update :broadcast_status
+
+  private
+
+  def broadcast_status
+    return unless confirmed_changed?
+
+    ActionCable.server.broadcast(
+      "booking_status_#{id}",
+      booking_id: id,
+      confirmed: confirmed
+    )
+  end
 end
+
+#[ActionCable] Broadcasting to booking_status:booking_status_7: {:booking_id=>7, :current_user_id=>12, :confirmed=>true}
+#[ActionCable] Broadcasting to booking_status:7: {:booking_id=>7, :current_user_id=>12, :confirmed=>true}
